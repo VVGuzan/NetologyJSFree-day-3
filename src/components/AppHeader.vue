f1ba9670dca1250115d3f63a1d2b3bbbcdb51418
VVGuzan <izvoznick@ngs.ru>
2021-08-25 16:44:59 +0700

    copy from day-2

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Marvel</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Поиск..." aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">все</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
